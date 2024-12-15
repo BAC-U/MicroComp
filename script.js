969db161400c552eef850c2fae3e8abf138db3e4
John <81330397+BananasMonkey@users.noreply.github.com>
2024-12-14 19:19:58 -0800

    d

--- script.js
+++ script.js
@@ -1,52 +1,38 @@
-function getAllBlocks(workspace) {
-    let blockInfo = [];
-    let ids = new Set();
-
-    function getBlockInfo(block) {
-        if (ids.has(block.id)) return null;
-        ids.add(block.id);
-
-        // Collect nested blocks (true child blocks inside inputs)
-        let nbf = [];
-        block.inputList.forEach(input => {
-            if (input.connection && input.connection.targetBlock()) {
-                let targetBlock = getBlockInfo(input.connection.targetBlock());
-                if (targetBlock) {
-                    nbf.push(targetBlock);
-                }
+function compileWorkspaceToJson() {
+    const blocks = workspace.getAllBlocks();
+    let bl2 = [];
+    let ids = [];
+    console.clear();
+    blocks.forEach(block => {
+        let bl = {};
+        bl = {
+            id: block.id,
+            type: block.type,
+            child: []
+        }
+        if (block.getSurroundParent()) {
+            bl.ptype = block.getSurroundParent().type;
+            bl.pid = block.getSurroundParent().id;
+        } else {
+            bl.ptype = "";
+            bl.pid = "null"
+        }
+        console.log(bl);
+        bl2.push(bl); // use conact to keeep list reversedd
+    });
+    bl2.forEach(block => {
+        let id = block.pid;
+        bl2.forEach(block2 => {
+            if (block2.id == id) {
+                block2.child.concat(block);
+                ids.concat(block.id);
             }
         });
-
-        // Process the next block (sequential sibling block)
-        let nextBlock = block.getNextBlock();
-        while (nextBlock) {
-            let nextBlockData = getBlockInfo(nextBlock);
-            if (nextBlockData) {
-                blockInfo.push(nextBlockData);  // Push sequential sibling block to the top-level array
-            }
-            nextBlock = nextBlock.getNextBlock();
-        }
-
-        return {
-            type: block.type,
-            id: block.id,
-            childs: nbf
-        };
-    }
-
-    // Process all top-level blocks
-    workspace.getTopBlocks(true).forEach(block => {
-        let blockData = getBlockInfo(block);
-        if (blockData) {
-            blockInfo.push(blockData);
-        }
     });
-
-    return blockInfo;
-}
-
-function compileBlocksToJson() {
-    let blocksJson = getAllBlocks(workspace);
-    console.log(JSON.stringify(blocksJson, null, 2));
-    console.log(workspace.getAllBlocks(false));
+    for (let i = 0; i > bl2.length; i++) {
+        if (ids.includes(bl2[i].id)) {
+            bl2.splice(i,1);
+        }
+    }
+    console.log(JSON.stringify(bl2, null, 2));
 }
